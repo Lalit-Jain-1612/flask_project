44c9495468e4b6df36efaba9eacc4b32a9256696
Ubuntu <ubuntu@ip-172-31-39-104.ec2.internal>
2024-04-10 07:20:38 +0000

    added 4 files

---
+++ flask_app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+tasks = ["lalit"]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    task = request.json
+    tasks.append(task)
+    return jsonify(task), 201
+
+#if __name__ == '__main__':
+ #   app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
